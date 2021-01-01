58abee3934510e8d77f51cd214bfd6766c773e47
Sandis <sandissirmais8@gmail.com>
2021-01-01 17:55:43 +0200

    Uzlabojumi
    + Dropdown menu padding fix
    + Texta paddingi dažādos izmēros
    + Video poga nospiežama
    + Email poga hover, active, margini
    + 480px galerijas atēli palielināti
    + 480px Atbalstītāju un footer sadaļas izmēri izlaboti
    + No header izņemta video sekcija
    + No footer izņemta atbalstītāju sekcija
    + Kājenes navigācijai hover effekts
    + izveidots anchor tag, kas parādās skrollējot

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -38,3 +38,18 @@ $(document).ready(function(){
     }).resize(); // This will simulate a resize to trigger the initial run.
 });
 
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40 ) {
+            $('#topBtn').fadeIn();
+        } else{
+            $('#topBtn').fadeOut();
+        }
+    });
+
+$("#topBtn").click(function(){
+    $('html, body').animate({scrollTop : 0}, 800);
+    });
+});
