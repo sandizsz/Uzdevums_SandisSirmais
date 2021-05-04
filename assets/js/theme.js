9e0b85f02ca32f4f4a70169c924b5b8f6874b9eb
Sandis <sandissirmais8@gmail.com>
2021-05-04 12:32:45 +0300

    Cookies

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -58,6 +58,38 @@ function validateForm() {
 
 }
 
+
+// Cookie policy //
+
+
+    const cookieBox = document.querySelector(".wrapper"),
+    acceptBtn = cookieBox.querySelector("button");
+    declineBtn = cookieBox.querySelector(".decline");
+
+    declineBtn.onclick = ()=>{
+    
+      if(document.cookie){ //if cookie is set
+      
+      }else{ //if cookie not set then alert an error
+        cookieBox.classList.add("hide"); //hide cookie box
+      }
+    }
+   
+
+
+    acceptBtn.onclick = ()=>{
+      //setting cookie for 1 month, after one month it'll be expired automatically
+      document.cookie = "CookieBy=SparkleHeart; max-age="+60*60*24*30;
+      if(document.cookie){ //if cookie is set
+        cookieBox.classList.add("hide"); //hide cookie box
+      }else{ //if cookie not set then alert an error
+        alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+      }
+    }
+    let checkCookie = document.cookie.indexOf("CookieBy=SparkleHeart"); //checking our cookie
+    //if cookie is set then hide the cookie box else show it
+    checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
+
 // Validate form and push out a message when the button 'Sūtīt' is clicked
 
 
